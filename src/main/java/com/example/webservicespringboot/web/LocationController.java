0b4cd4074ec1bc63f685039fb576db928f72298c
Thanh Nguyen <vnscriptkid@gmail.com>
2019-10-10 15:25:44 +0700

    Add Location Controller to handle requests

---
+++ src/main/java/com/example/webservicespringboot/web/LocationController.java
@@ -0,0 +1,28 @@
+package com.example.webservicespringboot.web;
+
+import java.util.List;
+
+import com.example.webservicespringboot.entity.Location;
+import com.example.webservicespringboot.service.LocationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LocationController {
+  private LocationService locationService;
+
+  @Autowired
+  public void setLocationService(LocationService locationService) {
+    this.locationService = locationService;
+  }
+
+  @GetMapping("/locations")
+  public ResponseEntity<List<Location>> getAllLocations() {
+    List<Location> list = locationService.retrieveLocations();
+    return new ResponseEntity<List<Location>>(list, HttpStatus.OK);
+  }
+}
