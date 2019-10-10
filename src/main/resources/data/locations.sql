2d2f23d5a77aabd053b035c0cd4867ae2637f69d
Thanh Nguyen <vnscriptkid@gmail.com>
2019-10-10 15:21:44 +0700

    Feed initial data for h2 db

---
+++ src/main/resources/data/locations.sql
@@ -0,0 +1,12 @@
+INSERT INTO location
+  (name, address)
+VALUES
+  ('Vinhomes', 'Lieu Giai, Ba Dinh, Ha Noi');
+INSERT INTO location
+  (name, address)
+VALUES
+  ('Royal City', 'Nguyen Trai, Thanh Xuan, Ha Noi');
+INSERT INTO location
+  (name, address)
+VALUES
+  ('Times City', 'Minh Khai, Hai Ba Trung, Ha Noi');
