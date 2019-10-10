d33c426db5ef8c13d783b0af578b1900c37e3e3d
Thanh Nguyen <vnscriptkid@gmail.com>
2019-10-10 15:22:51 +0700

    Add Entity Layer that represents a table in db

---
+++ src/main/java/com/example/webservicespringboot/entity/Location.java
@@ -0,0 +1,46 @@
+package com.example.webservicespringboot.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Location {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+  private String address;
+
+  public Location(Long id, String name, String address) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+  }
+
+  public Location(String name, String address) {
+    this.name = name;
+    this.address = address;
+  }
+
+  public Location() {
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAddress() {
+    return this.address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+}
