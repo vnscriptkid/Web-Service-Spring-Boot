8e2b6e5de588218ce390a9637443415a444ff368
Thanh Nguyen <vnscriptkid@gmail.com>
2019-10-10 12:15:30 +0700

    Start webService project

---
+++ src/test/java/com/example/webservicespringboot/WebserviceSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.webservicespringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebserviceSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
